4564244f39a38f62b316fc9eb92c5176eeaa5313
Christian N. Kimbi <christiankimbi@gmail.com>
2019-04-19 19:14:52 +0200

    Started the app, created the models, and register controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NG_Core_Auth.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NG_Core_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formData) 
+        {
+            
+
+            return Ok();
+
+        }
+    }
+}
